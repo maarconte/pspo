a4f02429d01317315cd239a5d6f4516239c196da
Mathilde Arconte <signup@marconte.fr>
2024-11-04 23:07:11 +0100

    Create questions table

---
+++ src/components/TableQuestions/TableQuestions.tsx
@@ -0,0 +1,15 @@
+import './style.scss';
+import './style-mobile.scss';
+
+import React, { FC, useState, useEffect } from 'react';
+import { TableQuestionsProps } from './TableQuestions.types';
+
+const TableQuestions: FC<TableQuestionsProps> = () => {
+  return (
+    <div className="TableQuestions">
+      TableQuestions
+    </div>
+  );
+}
+
+export default TableQuestions;
---
+++ src/pages/EditQuestions.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TableQuestions from "../components/TableQuestions";
+type Props = {};
+
+export default function EditQuestions({}: Props) {
+  return (
+    <div>
+      <h1>Edit Questions</h1>
+      <TableQuestions />
+    </div>
+  );
+}
