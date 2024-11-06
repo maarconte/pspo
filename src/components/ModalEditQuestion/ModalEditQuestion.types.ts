6f848de2e861b6277cfc792e697f21c74e32d698
Mathilde Arconte <signup@marconte.fr>
2024-11-07 00:13:43 +0100

    Add questions

--- src/components/ModalEditQuestion/ModalEditQuestion.types.ts
+++ src/components/ModalEditQuestion/ModalEditQuestion.types.ts
@@ -3,5 +3,5 @@ import { Question } from "../../utils/types";
 export interface ModalEditQuestionProps {
   isOpen: boolean;
   setIsOpen: (isOpen: boolean) => void;
-  question: Question;
+  question?: Question;
 }
--- src/pages/EditQuestions.tsx
+++ src/pages/EditQuestions.tsx
@@ -1,7 +1,12 @@
+import Button from "../components/Button";
+import { Button_Style } from "../components/Button/Button.types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import ModalEditQuestion from "../components/ModalEditQuestion";
 import React from "react";
 import TableQuestions from "../components/TableQuestions";
-
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 export default function EditQuestions() {
+  const [isAddModalOpen, setIsAddModalOpen] = React.useState(false);
   return (
     <div className="EditQuestions">
       <div className="container">
@@ -12,6 +17,23 @@ export default function EditQuestions() {
 			  - Total questions flagged
 			  - Total questions missing feedback
 			  */}
+        <div className="d-flex gap-05 justify-content-end mb-1">
+          <Button
+            onClick={() => setIsAddModalOpen(true)}
+            label="Import questions"
+            icon={<FontAwesomeIcon icon={faPlus} />}
+          />
+          <Button
+            style={Button_Style.OUTLINED}
+            onClick={() => setIsAddModalOpen(true)}
+            label="Add a question"
+            icon={<FontAwesomeIcon icon={faPlus} />}
+          />
+        </div>
+        <ModalEditQuestion
+          isOpen={isAddModalOpen}
+          setIsOpen={setIsAddModalOpen}
+        />
         <TableQuestions />
       </div>
     </div>
