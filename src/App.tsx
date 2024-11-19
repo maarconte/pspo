d8d8e3ced981a4fc6dfc7bb2f0d8171059aea5b5
Mathilde Arconte <signup@marconte.fr>
2024-11-19 16:59:05 +0100

    Prevent refetch on window focus

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,9 @@
 import "./assets/scss/style.scss";
 
 import { HashRouter, Route, Routes } from "react-router-dom";
-import { QueryClient, QueryClientProvider, useQuery } from "react-query";
-import React, { useEffect, useState } from "react";
-import {
-  addDoc,
-  collection,
-  doc,
-  getDocs,
-  getFirestore,
-  setDoc,
-} from "firebase/firestore";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 import EditQuestions from "./pages/EditQuestions";
-import { Firebase } from "./firebase.js";
 import Home from "./pages/Home";
 import { QuestionsProvider } from "./utils/context";
 import Quizz from "./pages/Quizz";
@@ -21,25 +11,6 @@ import Quizz from "./pages/Quizz";
 const queryClient = new QueryClient();
 
 function App() {
-  const db = getFirestore(Firebase);
-  const questionsCollection = collection(db, "questions");
-
-  // const addQuestions = async () => {
-  //   try {
-  //     questionsJSON.forEach(async (question: any) => {
-  //       await addDoc(questionsCollection, question);
-  //     });
-  //     console.log("success");
-  //   } catch (e) {
-  //     console.error("Error adding document: ", e);
-  //   }
-  // };
-
-  useEffect(() => {
-    // only add questions once
-    // addQuestions();
-  }, []);
-
   return (
     <QueryClientProvider client={queryClient}>
       <QuestionsProvider>
