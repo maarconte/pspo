3e71682655e95d27fdeae36b64394576d07b5a34
Mathilde Arconte <signup@marconte.fr>
2024-11-21 16:46:18 +0100

    Ajout du composant Header et de la page de connexion, mise à jour des styles et tests associés

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import { HashRouter, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 import EditQuestions from "./pages/EditQuestions";
+import Header from "./components/Header";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 import { QuestionsProvider } from "./utils/context";
 import Quizz from "./pages/Quizz";
 
@@ -15,10 +17,12 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <QuestionsProvider>
         <HashRouter>
+          <Header />
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/quizz" element={<Quizz />} />
             <Route path="/admin" element={<EditQuestions />} />
+            <Route path="/login" element={<Login />} />
           </Routes>
         </HashRouter>
       </QuestionsProvider>
