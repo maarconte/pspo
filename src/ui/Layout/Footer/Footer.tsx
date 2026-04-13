30f9f1d684ca390eb36f5885431c107a0d6b640f
Mathilde Arconte <signup@marconte.fr>
2026-04-13 11:19:28 +0200

    feat: translate footer content to English and improve code formatting

--- src/ui/Layout/Footer/Footer.tsx
+++ src/ui/Layout/Footer/Footer.tsx
@@ -16,37 +16,46 @@ const Footer = () => {
     <footer className="Footer">
       <div className="footer-container">
         <div className="footer-section">
-          <img src={logo} alt="Logo" className='logo' />
-          <p>Préparez votre certification Agile avec confiance.</p>
+          <img src={logo} alt="Logo" className="logo" />
+          <p>Prepare for your Agile certification with confidence.</p>
         </div>
         <div className="footer-section">
-          <h4 className='mb-1'>Liens utiles</h4>
-          <ul className='footer-nav'>
+          <h4 className="mb-1">Useful links</h4>
+          <ul className="footer-nav">
             <li>
-              <Link to="https://www.scrum.org/" target="_blank">Certification (Scrum.org)</Link>
+              <Link to="https://www.scrum.org/" target="_blank">
+                Certification (Scrum.org)
+              </Link>
             </li>
             <li>
-              <Link to="https://www.scrum.org/open-assessments" target="_blank">Open Assessments (Scrum.org)</Link>
+              <Link to="https://www.scrum.org/open-assessments" target="_blank">
+                Open Assessments (Scrum.org)
+              </Link>
             </li>
             <li>
-              <Link to="https://scrumguides.org/download.html" target="_blank">Scrum Guide</Link>
+              <Link to="https://scrumguides.org/download.html" target="_blank">
+                Scrum Guide
+              </Link>
             </li>
           </ul>
         </div>
 
         <div className="footer-section donation">
-          <p>Soutenez le projet</p>
-            <Button
-                url="https://www.paypal.com/donate/?hosted_button_id=QZCENBKK9TN6E"
-                icon={<Heart size={16} />}
-                type={Button_Type.WARNING}
-                style={Button_Style.TONAL}
-                label="Donate"
-              />
+          <p>Support the project</p>
+          <Button
+            url="https://www.paypal.com/donate/?hosted_button_id=QZCENBKK9TN6E"
+            icon={<Heart size={16} />}
+            type={Button_Type.WARNING}
+            style={Button_Style.TONAL}
+            label="Donate"
+          />
         </div>
 
         <div className="footer-bottom">
-          <p>{new Date().getFullYear()} Study Group. Fait avec passion pour la communauté Agile.</p>
+          <p>
+            {new Date().getFullYear()} Study Group. Made with passion for the
+            Agile community.
+          </p>
         </div>
       </div>
     </footer>
