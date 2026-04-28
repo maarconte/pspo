565ba8925e33919a194c018d8abcda67a7b66cf6
Mathilde Arconte <signup@marconte.fr>
2026-04-28 16:15:16 +0200

    Updates

--- assets/StatCard-CgMHAb_I.js
+++
@@ -1,7 +0,0 @@
-import{c as n,j as e}from"./index-wiaDCPAa.js";/**
- * @license lucide-react v0.561.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const d=[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["path",{d:"m15 9-6 6",key:"1uzhvr"}],["path",{d:"m9 9 6 6",key:"z0biqf"}]],o=n("circle-x",d),m=({value:c,label:t,icon:a,variant:s="default",className:r="",isFeatured:i=!1})=>e.jsxs("div",{className:`stat-card ${s!=="default"?s:""} ${i?"is-featured":""} ${r}`.trim(),children:[e.jsx("div",{className:"icon-container",children:a}),e.jsxs("div",{className:"stat-content",children:[e.jsx("strong",{children:c}),e.jsx("span",{children:t})]})]});export{o as C,m as S};
-//# sourceMappingURL=StatCard-CgMHAb_I.js.map
