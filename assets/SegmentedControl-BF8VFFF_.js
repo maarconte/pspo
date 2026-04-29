46521123cc58c8e5be09c5a907ad77245d0f78e2
Mathilde Arconte <signup@marconte.fr>
2026-04-29 16:59:55 +0200

    Updates

---
+++ assets/SegmentedControl-BF8VFFF_.js
@@ -0,0 +1,7 @@
+import{c as d,j as s}from"./index-BCUUIX-Z.js";/**
+ * @license lucide-react v0.561.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const r=[["path",{d:"M12 6v6l4 2",key:"mmk7yg"}],["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}]],t=d("clock",r),i=({options:a,value:c,onChange:l,name:n})=>s.jsxs("div",{className:"segmented-control",children:[a.map(e=>s.jsxs("label",{className:`segmented-item ${c===e.value?"active":""}`,children:[s.jsx("input",{type:"radio",name:n,value:e.value,checked:c===e.value,onChange:()=>l(e.value)}),s.jsxs("div",{className:"segmented-label",children:[e.icon&&s.jsx("span",{className:"segmented-icon",children:e.icon}),s.jsx("span",{children:e.label})]})]},e.value)),s.jsx("div",{className:"segmented-slider",style:{width:`calc(${100/a.length}% - 4px)`,transform:`translateX(calc(${a.findIndex(e=>e.value===c)*100}%))`}})]});export{t as C,i as S};
+//# sourceMappingURL=SegmentedControl-BF8VFFF_.js.map
---
+++ assets/analytics-M5571rse.js
@@ -0,0 +1,2 @@
+const n=(t,e)=>{typeof gtag>"u"||gtag("event",t,e)};export{n as t};
+//# sourceMappingURL=analytics-M5571rse.js.map
